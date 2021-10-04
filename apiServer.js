a9e8558396d26ddae0c7ec346735e22e55346bc7
krokyk <peter.krokavec@gmail.com>
2021-10-04 20:58:21 +0200

    step 05

--- apiServer.js
+++ apiServer.js
@@ -5,8 +5,14 @@ const express = require("express")
 const app = express()
 
 const posts = [
-    { author: "Jane", title: "Post 1" },
-    { author: "John", title: "Post 2" }
+    {
+        author: "Jane",
+        title: "Post 1"
+    },
+    {
+        author: "John",
+        title: "Post 2"
+    }
 ]
 
 app.get('/posts', (req, res) => {
